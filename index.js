dde980168ecde7082888ee24683a68f812e6f21c
kabdulka <kenabdulka@gmail.com>
2023-03-22 14:55:06 -0700

    add inventories route in index.js

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const PORT = process.env.PORT || 5050;
 //   res.send('Welcome to my API');
 // });
 const warehouseRoutes = require('./routes/warehouseRoute');
+const inventoryRoutes = require('./routes/inventoryRoute');
 
 app.use('/warehouses', warehouseRoutes);
-
+app.use('/inventories', inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
