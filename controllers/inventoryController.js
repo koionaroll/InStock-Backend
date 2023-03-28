5f8547153d6042ad8d164ccbe437c4fadad44a3f
kabdulka <kenabdulka@gmail.com>
2023-03-27 23:41:09 -0700

    fix add inventory backend request bug

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,5 @@
 const knex = require('knex')(require('../knexfile'));
+const { v4: uuidv4 } = require('uuid');
 
 exports.index = (_req, res) => {
    knex('inventories')
@@ -34,6 +35,50 @@ exports.deleteInventory = (req, res) => {
       .catch((err) =>
         res.status(400).send(`Error deleting Inventory ${req.params.id} ${err}`)
       );
-  };
+};
+
+exports.newInventory = (req, res) => {
+
+
+    let newItem = req.body;
+
+    if (!newItem.item_name || !newItem.description || !newItem.category || !newItem.status || !newItem.quantity) {
+        return res.status(400).send(`Could not add a new inventory item. Make sure to fill in all of the missing fields`);
+    }
+
+    // give the new inventory item a new id
+    newItem.id = uuidv4();
+    knex("inventories")
+    .insert(newItem)
+    .then(data => {
+        res.status(201).send(`inventory successfully created and added to warehosue with Id ${req.params.id}`);
+    })
+    .catch(err => {
+        console.log(`Could not add new inventory item ${err}`);
+    })
+}
+
+exports.editInventory = (req, res) => {
+
+    let updatedItem = req.body;
+    let warehouseId = req.params.id;
+
+    if (!updatedItem.item_name || !updatedItem.description || !updatedItem.category || !updatedItem.status
+        || !updatedItem.quantity) {
+        return res.status(400).send(`Counld not edit inventory Item. Please fill in all of the missing fields`);
+    }
+
+    updatedItem.id = warehouseId;
+
+    knex("inventories")
+    .update(updatedItem)
+    .where()
+    .then(() => {
+
+    })
+    .catch(err => {
+        console.log(`Error in updating inventory item ${err}`)
+    })
+}
 
 
